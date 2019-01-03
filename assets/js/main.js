78ace3b65b21311547f385fac6bad8e97d149628
Yulee Foster <yuleefoster@Yulees-MBP.home>
2019-01-03 18:29:19 +0000

    Added the jump

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,18 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+function setClipboard(value) {
+    var tempInput = document.createElement("input");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+}
+
+
+
 
 (function($) {
 
